127475a7ddd7f9c921a6bba539a14bb4884dd4f5
crumblingstatue <radiantstatue@gmail.com>
2026-02-22 17:14:16 +0100

    Add `Units::enumerated`

--- src/herd.rs
+++ src/herd.rs
@@ -199,7 +199,15 @@ impl Units {
     pub const fn is_empty(&self) -> bool {
         self.len() == 0
     }
-    /// Iterate over the units, along with their indices
+    /// Immutably iterate over the units, along with their indices
+    pub fn enumerated(&self) -> impl Iterator<Item = (UnitIdx, &Unit)> {
+        #[expect(
+            clippy::cast_possible_truncation,
+            reason = "50 is the max unit number, so this always succeeds"
+        )]
+        self.iter().enumerate().map(|(idx, item)| (UnitIdx(idx as u8), item))
+    }
+    /// Mutably iterate over the units, along with their indices
     pub fn enumerated_mut(&mut self) -> impl Iterator<Item = (UnitIdx, &mut Unit)> {
         #[expect(
             clippy::cast_possible_truncation,
